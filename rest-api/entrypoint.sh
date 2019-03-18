fcb228f922a365cd3feb2be51aa452eeeaa84b1f
Youchen Du <youchen.du@gmail.com>
2019-03-18 10:40:45 +0800

    Update configs to support LB.

--- rest-api/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Apply database migrations
-echo "Apply database migrations"
-python manage.py migrate
-
-# Start server
-echo "Starting server"
-uwsgi --socket :8080 --ini /uwsgi.ini
