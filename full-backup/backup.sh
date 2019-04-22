d2ddaae0dbfef427150a08eb3d706d0f71127e75
Youchen Du <youchen.du@gmail.com>
2019-04-22 20:22:04 +0800

    Add service for backup.

---
+++ full-backup/backup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+[ -z "${MYSQL_USER}" ] && { echo "=> MYSQL_USER cannot be empty" && exit 1; }
+[ -z "${MYSQL_PASS:=$MYSQL_PASSWORD}" ] && { echo "=> MYSQL_PASS cannot be empty" && exit 1; }
+
+DATE=$(date +%Y%m%d%H%M)
+echo "=> Backup started at $(date "+%Y-%m-%d %H:%M:%S")"
+databases=${MYSQL_DATABASE:-${MYSQL_DB:-$(mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)}}
+BASEDIR=/backup/"$DATE"
+mkdir $BASEDIR
+for db in $databases
+do
+  if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]]
+  then
+    echo "==> Dumping database: $db"
+    FILENAME=$BASEDIR/$db.sql
+    LATEST=/backup/latest.$db.sql.gz
+    if mysqldump -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u "$MYSQL_USER" -p"$MYSQL_PASS" --databases "$db" $MYSQLDUMP_OPTS > "$FILENAME"
+    then
+      gzip -f "$FILENAME"
+      echo "==> Creating symlink to latest backup: $(basename "$FILENAME".gz)"
+      rm "$LATEST" 2> /dev/null
+      ln -s "$FILENAME".gz "$LATEST"
+    else
+      rm -rf "$DATE"
+    fi
+  fi
+done
+echo "==> Copying media files"
+cp -r /media $BASEDIR
+cd $BASEDIR
+tar -zcf media.tar.gz media
+rm -rf media
+cd /
+echo "==> Media files copied"
+
+if [ -n "$MAX_BACKUPS" ]
+then
+  echo "=> Max number of backups ("$MAX_BACKUPS") reached. Deleting oldest backups"
+  while [ "$(find /backup -maxdepth 1 -mindepth 1 | wc -l)" -gt "$MAX_BACKUPS" ]
+  do
+    TARGET=$(find /backup -maxdepth 1 -mindepth 1 | sort | head -n 1)
+    rm -rf "$TARGET"
+    echo "==> Backup $TARGET has been deleted"
+  done
+fi
+
+echo "=> Backup process finished at echo $(date "+%Y-%m-%d %H:%M:%S")"
---
+++ full-backup/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+touch /mysql_backup.log
+tail -F /mysql_backup.log &
+
+# Read MySQL password from secret file.
+export MYSQL_PASS=$(egrep -v '^#' /run/secrets/mysql-root-password)
+
+if [ -n "${INIT_BACKUP}" ]; then
+  echo "=> Create a backup on the startup"
+  /backup.sh
+elif [ -n "${INIT_RESTORE_LATEST}" ]; then
+  echo "=> Restore latest backup"
+  until nc -z "$MYSQL_HOST" "$MYSQL_PORT"
+  do
+      echo "waiting database container..."
+      sleep 1
+  done
+find /backup -maxdepth 1 -name '*.sql.gz' | tail -1 | xargs /restore.sh
+fi
+
+echo "${CRON_TIME} /backup.sh >> /mysql_backup.log 2>&1" > /crontab.conf
+crontab /crontab.conf
+echo "=> Running cron task manager"
+exec crond -f
